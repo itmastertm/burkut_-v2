17e048ab9e396b5299de4b895536e66f814eda48
Vitaliy Vinnikov <itmastertm@gmail.com>
2020-10-18 14:08:48 -0700

    fix folder path

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "burkut"
+set :application, "www"
 set :repo_url, "https://github.com/itmastertm/burkut_v2.git"
 
 # Default branch is :master
